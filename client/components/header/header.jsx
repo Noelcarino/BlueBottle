b640bec99fb4f894ea07463428b10cfa11695141
Noelcarino <leononirac1992@gmail.com>
2020-07-22 06:51:12 +0000

    7/21 | completed basic header for blue bottle

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './header_styles.css';
 
 export default class Header extends React.Component {
     render(){
         return (
-            <div>
-                This is the header
+            <div className="header_container">
+                <p>Blue Bottle</p>
             </div>
         )
     }
